6a927a382e4b6473f1170e726443625584465b43
Patrick <patrickb97@hotmail.com>
2016-06-28 13:55:38 -0500

    Add files via upload

--- pythonIM.py
+++ pythonIM.py
@@ -1,9 +1,12 @@
 import socket
+import Tkinter
 import time
 s = socket.socket()
 c = socket.socket()
 
 userNum = input("Are you user 1 or 2: ")
+messages = ['']
+
 
 #Assign server/client IPs						
 if userNum == 1:
@@ -19,20 +22,38 @@ else:
 	clientPort = 12345
 	waitTime = 5
 
+screen = Tkinter.Tk()
+printuserNum = Tkinter.Label(text = ("User Number:" , userNum)); printuserNum.pack()
+printIP = Tkinter.Label(text = ("IP address:", serverHost)); printIP.pack()
+label1 = Tkinter.Label(text = messages[0]); label1.pack()
+label2 = Tkinter.Label(text = messages[0]); label2.pack()
+label3 = Tkinter.Label(text = messages[0]); label3.pack()
+label4 = Tkinter.Label(text = messages[0]); label4.pack()
+label5 = Tkinter.Label(text = messages[0]); label5.pack()
+label6 = Tkinter.Label(text = messages[0]); label6.pack()
+label7 = Tkinter.Label(text = messages[0]); label7.pack()
+label8 = Tkinter.Label(text = messages[0]); label8.pack()
+label9 = Tkinter.Label(text = messages[0]); label9.pack()
+screen.mainloop()
+	
+	
+	
+	
+	
 s.bind((serverHost, serverPort))   		
-s.listen(5)						#Listens for connection
+s.listen(5)							#Listens for connection
 print "Please wait..."
 time.sleep(waitTime)					#Waits for other connection to be open
-c.connect((clientHost, clientPort))			#Connects to other client
-oc, addr = s.accept()					#Accepts connection
+c.connect((clientHost, clientPort))		#Connects to other client
+oc, addr = s.accept()			#Accepts connection
 	
 
 while True:
 	if userNum == 1:
 		oc.send((raw_input("User 1: ") + " "), userNum)		#Sends Message
-		print "User 2:" , c.recv(1024)				#Receives Message
+		print "User 2:" , c.recv(1024)									#Receives Message
 	else:
-		print "User 1:" , c.recv(1024)				#Receives Message
+		print "User 1:" , c.recv(1024)									#Receives Message
 		oc.send((raw_input("User 2: ") + " "), userNum)		#Sends Message
  
  
@@ -49,8 +70,5 @@ Currently only allows one message to be send
 Deletes last character of one sided message
 
 Changes:
--Added Continual Messaging
--Alternates back and forth, waiting for message before sending one
--Cut waiting period in half
--Improvised fix for missing character
+-
 """
