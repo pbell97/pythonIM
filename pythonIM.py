d1c2e52544aca25a2a92c3b1d704e42a3d845df5
Patrick <patrickb97@hotmail.com>
2016-06-28 10:58:20 -0500

    Add files via upload

--- pythonIM.py
+++ pythonIM.py
@@ -12,9 +12,9 @@ c = socket.socket()
 
 userNum = input("Are you user 1 or 2: ")
 
-#Assign server/client IPs
+#Assign server/client IPs						
 if userNum == 1:
-	serverHost = '192.168.20.157'
+	serverHost = '192.168.20.157'		#Change IPs depending on use
 	serverPort = 12345
 	clientHost = '192.168.20.156'
 	clientPort = 12346
@@ -26,16 +26,16 @@ else:
 	clientPort = 12345
 	waitTime = 10
 
-s.bind((serverHost, serverPort))
-s.listen(5)
+s.bind((serverHost, serverPort))   		
+s.listen(5)							#Listens for connection
 print "Wait period..."
-time.sleep(waitTime)
-c.connect((clientHost, clientPort))
+time.sleep(waitTime)					#Waits for other connection to be open
+c.connect((clientHost, clientPort))		#Connects to other client
 
 while True:
-	oc, addr = s.accept()
-	oc.send(raw_input("Message Please: "), userNum)
-	print c.recv(1024)
+	oc, addr = s.accept()								#Accepts connection
+	oc.send(raw_input("Message Please: "), userNum)		#Sends Message
+	print c.recv(1024)									#Receives Message
 
 
 
