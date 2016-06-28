f411e1644a60aee306442c27f6ffbf3166c21595
Patrick <patrickb97@hotmail.com>
2016-06-28 10:50:35 -0500

    Notes Change
    Put notes at top, deleted unnecessary old/saved code at bottom.

--- pythonIM.py
+++ pythonIM.py
@@ -1,3 +1,10 @@
+"""
+pythonIM Notes:
+Use  'ps aux' in terminal to find current processes if pythonIM is still running
+Currently only allows one message to be send
+Deletes last character of one sided message
+"""
+
 import socket
 import time
 s = socket.socket()
@@ -35,32 +42,3 @@ while True:
 
 
 
-
-
-"""
-#Connects to server
-s = socket.socket()
-serverHost = '192.168.20.150'
-serverPort = 12345
-s.connect((serverHost, serverPort))
-s.recv(1024)
-
-
-c = socket.socket()
-clientHost = '192.168.20.156'
-clientPort = 12346
-c.bind((clientHost, clientPort)) 
-c.listen(5)
-while True
-	newText = raw_input("What would you like to send: ")
-	c.send(newText
-	
-	c, addr = s.accept()
-	print 'Got connection from', addr
-	c.send('Thank you for connecting, fam')
-	print "c is:", c
-	c.close()
-"""
-
-
-#Use  'ps aux' in terminal to find current processes if pythonIM is still running
