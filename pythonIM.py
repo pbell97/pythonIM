4255f55c423d3de6c184f12c6f3601687bb51e2c
Patrick <patrickb97@hotmail.com>
2016-06-29 12:37:15 -0500

    WIP Full Gui

--- pythonIM.py
+++ pythonIM.py
@@ -5,67 +5,77 @@ s = socket.socket()
 c = socket.socket()
 userNum = 0
 
+
+def assignIPs():			#Assigns the correct server/client IPs
+	global serverHost, serverPort, clientHost, clientPort, waitTime
+	#Assign server/client IPs	
+	if userNum == 1:
+		serverHost = '192.168.20.157'		#Change IPs depending on use
+		serverPort = 12345
+		clientHost = '192.168.20.156'
+		clientPort = 12346
+		waitTime = 2.5
+	else:
+		serverHost = '192.168.20.156'
+		serverPort = 12346
+		clientHost = '192.168.20.157'
+		clientPort = 12345
+		waitTime = 5
 def assignUser1():
 	global userNum
 	userNum = 1
 	print userNum
-	selector.destroy()
+	assignIPs()
 def assignUser2():
 	global userNum
 	userNum = 2
 	print userNum
-	selector.destroy()
-	
+	assignIPs()
+def newEntry(): #Gets text from field
+	global message;
+	message = field.get()
+	field.delete( 0, len(message))
+def startServer():
+	s.bind((serverHost, serverPort))   		
+	s.listen(5)							#Listens for connection
+def acceptConnection():
+	oc, addr = s.accept()			#Accepts connection
+def connectClient():
+	c.connect((clientHost, clientPort))		#Connects to other client
+def whileLoop():
+	while True:
+		if userNum == 1:
+			oc.send(("User 1: " + message + " "), userNum)		#Sends Message
+			print "User 2:" , c.recv(1024)									#Receives Message
+		else:
+			print "User 1:" , c.recv(1024)									#Receives Message
+			oc.send(("User 2: " + message + " "), userNum)		#Sends Message
+
 
 selector = Tkinter.Tk()
-label1 = Tkinter.Label(text = "Select Which User You Are"); label1.pack(padx=5, pady=10, side="left")
-button1 = Tkinter.Button(text = "User 1", command = assignUser1); button1.pack(padx=5, pady=10, side="left")
-button2 = Tkinter.Button(text = "User 2", command = assignUser2); button2.pack(padx=5, pady=10, side="left")
+label1 = Tkinter.Label(text = "Select Which User You Are"); label1.pack(padx=5, pady=10, side="left")			#Label Of which user number
+button1 = Tkinter.Button(text = "User 1", command = assignUser1); button1.pack(padx=5, pady=10, side="left")			#User1 Button
+button2 = Tkinter.Button(text = "User 2", command = assignUser2); button2.pack(padx=5, pady=10, side="left")			#User2 Button
+field = Tkinter.Entry(bd = 5); field.pack(side = 'bottom')			#Entry Field
+button3 = Tkinter.Button(text = "Submit Data", command = newEntry); button3.pack(side = 'bottom')			#Assigns field to var
+button4 = Tkinter.Button(text = "Start Server", command = startServer); button4.pack(padx=5, pady=10, side="right") 			#Starts TCP Server
+button5 = Tkinter.Button(text = "Connect to Client", command = connectClient); button5.pack(padx=5, pady=10, side="right") 			#Starts TCP Server
+button6 = Tkinter.Button(text = "Start While Loop", command = whileLoop); button6.pack(padx=5, pady=10, side = "right")
+button7 = Tkinter.Button(text = "Connection Accept", command = acceptConnection); button7.pack(padx=5, pady=10, side = "right")
+
 selector.mainloop()
 
-#userNum = input("Are you user 1 or 2: ")
-messages = ['']
+
+#messages = ['']
 
 
-#Assign server/client IPs						
-if userNum == 1:
-	serverHost = '192.168.20.157'		#Change IPs depending on use
-	serverPort = 12345
-	clientHost = '192.168.20.156'
-	clientPort = 12346
-	waitTime = 2.5
-else:
-	serverHost = '192.168.20.156'
-	serverPort = 12346
-	clientHost = '192.168.20.157'
-	clientPort = 12345
-	waitTime = 5
+
 
 
 	
 	
 	
 	
-	
-s.bind((serverHost, serverPort))   		
-s.listen(5)							#Listens for connection
-print "Please wait..."
-time.sleep(waitTime)					#Waits for other connection to be open
-c.connect((clientHost, clientPort))		#Connects to other client
-oc, addr = s.accept()			#Accepts connection
-	
-
-while True:
-	if userNum == 1:
-		oc.send((raw_input("User 1: ") + " "), userNum)		#Sends Message
-		print "User 2:" , c.recv(1024)									#Receives Message
-	else:
-		print "User 1:" , c.recv(1024)									#Receives Message
-		oc.send((raw_input("User 2: ") + " "), userNum)		#Sends Message
- 
- 
-
-
 
 
 
@@ -77,5 +87,5 @@ Currently only allows one message to be send
 Deletes last character of one sided message
 
 Changes:
-- User Selector Created
+-
 """
